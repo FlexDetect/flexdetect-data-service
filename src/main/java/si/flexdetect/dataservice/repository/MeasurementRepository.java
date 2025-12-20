c0cca8c62497bdb58d40dea7b96f07b3202909c1
aljazbrodar. <aljaz.brodar@gmail.com>
2025-12-20 16:43:56 +0100

    naming confusion in dataset repo and measurement repo

--- src/main/java/si/flexdetect/dataservice/repository/MeasurementRepository.java
+++ src/main/java/si/flexdetect/dataservice/repository/MeasurementRepository.java
@@ -4,7 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import si.flexdetect.dataservice.model.Measurement;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer> {
 
+    Optional<Measurement> findByIdAndDataset_IdAndDataset_Facility_UserId(Integer measurementId, Integer datasetId, Integer userId);
+
+    List<Measurement> findByDataset_IdAndDataset_Facility_UserId(Integer datasetId, Integer userId);
+
+    int deleteByIdAndDataset_IdAndDataset_Facility_UserId(Integer measurementId, Integer datasetId, Integer userId);
+
+    List<Measurement> findByDataset_Facility_IdAndDataset_Facility_UserId(Integer facilityId, Integer userId);
 }
--- src/main/java/si/flexdetect/dataservice/service/MeasurementService.java
+++ src/main/java/si/flexdetect/dataservice/service/MeasurementService.java
@@ -1,4 +1,25 @@
 package si.flexdetect.dataservice.service;
 
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import si.flexdetect.dataservice.model.Measurement;
+import si.flexdetect.dataservice.repository.MeasurementRepository;
+
+@Service
+@Transactional
 public class MeasurementService {
+
+    private final MeasurementRepository measurementRepository;
+
+    public MeasurementService(MeasurementRepository measurementRepository) {
+        this.measurementRepository = measurementRepository;
+    }
+
+    public Measurement createMeasurement(Measurement measurement) {
+        return measurementRepository.save(measurement);
+    }
+
+    public
+
 }
